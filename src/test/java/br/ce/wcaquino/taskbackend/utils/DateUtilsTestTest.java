9058b546c180422c254644a10e9f417b7ad8cd72
luccazx <Poterzil@1>
2020-07-06 21:40:53 -0300

    Testess unitários da aplicação

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTestTest.java
@@ -0,0 +1,14 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DateUtilsTestTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
